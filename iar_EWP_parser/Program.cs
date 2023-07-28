b0e71b992e0e3def98944dbd92e2e2d60ef35306
Heorhii Levinson <levinson.h@ajax.systems>
2023-07-28 13:00:11 +0300

    Initial commit. Added .gitignore template for C# console app. Created console app, hardcoded path to ewp prj file, testes that System.Xml works with .ewp files fine.

---
+++ iar_EWP_parser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+namespace iar_EWP_parser
+{
+    internal class Program
+    {
+        private static readonly string ewp_path = "C:\\Users\\user\\Source\\device1\\Devices1 PRJ_3.11.ewp";
+        static void Main(string[] args)
+        {
+            var proj_obj = new IAR_Project(ewp_path);
+            Console.WriteLine("End of execution");
+        }
+    }
+
+    public class IAR_Project
+    {
+        public XmlNode Workspace { get; set; }
+
+        public IAR_Project(string ewp_path)
+        {
+            XmlReader reader = XmlReader.Create(ewp_path);
+            XmlDocument eww = new XmlDocument();
+            eww.Load(reader);
+            reader.Close();
+
+            Workspace = eww.DocumentElement;
+
+            // Find the configuration in args[1] in the eww file
+            //XmlNodeList projects = workspace.SelectNodes("project");
+        }
+    }
+}
